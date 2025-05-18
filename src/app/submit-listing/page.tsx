45d5a5c9a818241bfdbf4f04a98f17feb0a200ca
Sanchit Bajaj <sanchit02@outlook.com>
2025-05-18 21:42:11 +0530

    submit listing back button

--- src/app/submit-listing/page.tsx
+++ src/app/submit-listing/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import Link from "next/link";
 import VehicleListingForm from "@/components/submit-listing/vehicle-listing-form";
+import { ChevronLeft } from "lucide-react";
 
 export default function SubmitListing() {
   return (
-    <main className="py-8 px-4">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6">Add New Vehicle</h1>
+    <main className="py-20 px-8 lg:px-2">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex flex-row gap-2 justify-start items-center mb-10">
+          <Link href="/">
+            <ChevronLeft size={36} />
+          </Link>
+          <h1 className="text-2xl lg:text-4xl font-bold text-white text-center lg:text-left">
+            Add New Vehicle
+          </h1>
+        </div>
         <VehicleListingForm />
       </div>
     </main>
